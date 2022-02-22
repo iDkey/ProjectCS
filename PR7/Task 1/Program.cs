bd3d603852fbe907d15ee0fdaf602ee6855e9e87
iDkey <dan.arxipow2011@ya.ru>
2022-02-22 14:48:59 +0300

    PR7

---
+++ PR7/Task 1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 0;
+            int K = 0;
+            string path = "";
+            const char ch = '*';
+            Console.WriteLine("Input a path to file:");
+            path = Console.ReadLine();
+            Console.WriteLine("Input count of lines:");
+            N = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input count of symbols:");
+            K = int.Parse(Console.ReadLine());
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    for (int i = 0; i < N; i++)
+                    {
+                        string line = "";
+                        for (int j = 0; j < K; j++)
+                        {
+                            line += ch;
+                        }
+                        sw.WriteLine(line);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+            
+        }
+    }
+}
---
+++ PR7/Task 2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathFileToRead = "";
+            string pathFileToWrite = "";
+            Console.WriteLine("Input a path to file 1:");
+            pathFileToWrite = Console.ReadLine();
+            Console.WriteLine("Input count of lines:");
+            pathFileToRead = Console.ReadLine();
+            string plug = "";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(pathFileToWrite, true, System.Text.Encoding.Default))
+                {
+                    using (StreamReader sr = new StreamReader(pathFileToRead))
+                    {
+                        plug = sr.ReadToEnd();
+                        sw.WriteLine(plug);
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+
+        }
+    }
+}
